07e1c8fe8237fc7dcd6adb54801b6e8c12aa67d9
Borisov Pavel <kayjaylitee@gmail.com>
2024-05-15 04:38:47 +0300

    Moved id, method and params to send_command

--- light.sh
+++ light.sh
@@ -24,16 +24,16 @@ ip=$1
 command=$2
 
 send_command() {
-    printf "$1\r\n" | nc -w1 $ip 55443
+    printf '{"id": 1, "method":"%s", "params":[%s]}\r\n' "$1" "$2" | nc -w1 $ip 55443
 }
 
 color_to_int() {
-    color_hex=$(grep -m1 -i "^# $1;" $0 | cut -d ';' -f 2)
-    printf '%d' $color_hex
+    color_hex=$(grep -m1 -i "^# $1;" "$0" | cut -d ';' -f 2)
+    printf '%d' "$color_hex"
 }
 
 # If the command is a known color name, assume the 'color' command
-if grep -m1 -q -i "^# $command;" $0; then
+if grep -m1 -q -i "^# $command;" "$0"; then
     color=$command
     command="color"
 else
@@ -42,28 +42,28 @@ fi
 
 case $command in
 "on"|"off")
- send_command '{"id":1,"method":"set_power","params":["'$command'","smooth",500]}'
+ send_command set_power '"'$command'","smooth",500'
  ;;
 "color")
- send_command '{"id":1,"method":"set_rgb","params":['$(color_to_int "$color")',\"smooth\",500]}'
+ send_command set_rgb ''$(color_to_int "$color")',"smooth",500'
  ;;
 "disco")
- send_command '{"id":1,"method":"start_cf","params":[ 50, 0, "100, 1, 255, 100, 100, 1, 32768, 100, 100, 1, 16711680, 100"]}'
+ send_command start_cf '50, 0, "100, 1, 255, 100, 100, 1, 32768, 100, 100, 1, 16711680, 100"'
  ;;
 "sunrise")
- send_command '{"id":1,"method":"start_cf","params":[ 3, 1, "50, 1, 16731392, 1, 360000, 2, 1700, 10, 540000, 2, 2700, 100"]}'
+ send_command start_cf '3, 1, "50, 1, 16731392, 1, 360000, 2, 1700, 10, 540000, 2, 2700, 100"'
  ;;
 "notify-"*)
  color=$(color_to_int ${command#notify-})
- send_command '{"id":1,"method":"start_cf","params":[ 5, 0, "100, 1, '$color', 100, 100, 1, '$color', 1"]}'
+ send_command start_cf '5, 0, "100, 1, '$color', 100, 100, 1, '$color', 1"'
  ;;
  "dim"|"undim")
  level=$([[ $command == "dim" ]] && echo 5 || echo 100)
- send_command '{"id":1,"method":"set_bright","params":['$level']}'
+ send_command  set_bright $level
  ;;
 "brightness")
  level=$3
- send_command '{"id":1,"method":"set_bright","params":['$level']}'
+ send_command  set_bright $level
  ;;
 *)
  printf "
@@ -72,15 +72,14 @@ light.sh [ip] [command] <color> -- utility to control Yeelight smart bulb over w
 where command can have one of the following values:
     on - turn on the light
     off - turn off the light
-    [color] <color> - set the color to %s 
+    [color] <color> - set the color to <color>
     disco - turns on disco mode
     sunrise - turns on sunrise mode
-    notify-blue - notification in blue color
-    notify-green - notification in green color
-    notify-red - notification in red color
+    notify-<color> - notification in <color>
     dim - dim light to brightness 5
     undim - reset light to brightness 100
     brightness <level> - set the brightness to <level> where <level> is an integer from 1 (dimmest) to 100 (brightest)
+    <colors>: %s
 " "$(awk '/^# Color values/ {flag=1; next} /^$/ {flag=0} flag' light.sh | cut -d ';' -f 1 | cut -d ' ' -f 2 | tr -s '\n' ',' | sed 's/,$//')"
 ;;
 esac
